0eaca7643d54f6d129e18b9f42a43822895f2530
Alicia Wheeler <wheeler.al@northeastern.edu>
2023-07-24 13:36:33 -0400

    Update streamlit_app.py
    added pandas and data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# using just streamlit
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -9,3 +10,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+# adding in python
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
