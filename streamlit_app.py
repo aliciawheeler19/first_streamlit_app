9eb6ded8c4e2958be4223a405171df8d4d550260
Alicia Wheeler <wheeler.al@northeastern.edu>
2023-07-25 14:02:49 -0400

    Update streamlit_app.py
    add in table of data from the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# import the data from fruityvice into a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates a view for the table on the page
+streamlit.dataframe(fruityvice_normalized)
+
