58c7599784671c40e65bad06f9506d35c1de9fd5
Alicia Wheeler <wheeler.al@northeastern.edu>
2023-07-27 16:04:57 -0400

    Update streamlit_app.py
    dc

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,6 @@ streamlit.dataframe(my_data_rows)
 # Add data into snowflake table
 # Textbox to ask which fruit the user is looking for
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute("insert into fruit_load_list values " + add_my_fruit )
-my_data_rows = my_cur.fetchall()
+my_cur.execute("insert into fruit_load_list values (" + add_my_fruit + ")")
 # Screen text to show what the user asked for
 streamlit.write('Thank for adding ', add_my_fruit)
