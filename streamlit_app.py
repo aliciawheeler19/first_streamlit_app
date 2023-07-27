c6d2c0cebe178e17ae25a41a7eeb0e2f60d757d9
Alicia Wheeler <wheeler.al@northeastern.edu>
2023-07-27 11:51:30 -0400

    Update streamlit_app.py
    fixed an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
